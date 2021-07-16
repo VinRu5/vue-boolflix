693d808ef14488d88a43c6db5e3265330af98aee
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-07-16 17:37:07 +0200

    layout

---
+++ src/components/Film.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="film-container">
+        <div class="film-inner">
+            <img :src="imgURL" :alt="`copertina di ${title}`">
+            <div class="opacity"></div>
+            <div class="info-container">
+                <div class="info-inner row">
+                    <div class="title-film col-12">{{ title }}</div>
+                    <div class="original-title-film col-7">{{ originalTitle }}</div>
+                    <div class="preferences col-3">
+                        <div class="vote-film">{{ vote }}</div>
+                        <div class="lang-film">{{ language }}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Film',
+    props: {
+        imgURL: String,
+        title: String,
+        originalTitle: String,
+        vote: Number,
+        language: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/colors';
+
+    .film-container {
+        color: $white;
+
+        .film-inner {
+            height: 350px;
+            width: 100%;
+            position: relative;
+            
+            &:hover {
+
+                .opacity {
+                    display: block;
+                }
+
+                .info-container {
+                    display: block;
+                }
+            }
+
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
+
+            .opacity {
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                height: 100%;
+                width: 100%;
+                padding: 12px 6px;
+                background: rgba(0, 0, 0, .3);
+                display: none;
+            }
+    
+            .info-container {
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                padding: 12px 6px;
+                display: none;
+
+    
+                .title-film {
+                    text-align: center;
+    
+                }
+    
+                .original-title-film {
+                    font-size: 12px;
+                    color: $link;
+                    text-align: center;
+                }
+
+                .preferences {
+                    font-size: 12px;
+                    text-align: right;
+                }
+            }
+
+        }
+    }
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+    <main class="row">
+        <Film v-for="film in films" :key="film.id"
+            :imgURL="film.poster_path"
+            :title="film.title"
+            :originalTitle="film.original_title"
+            :vote="film.vote_average"
+            :language="film.original_language"
+            class="col-2"
+        />
+    </main>
+</template>
+
+<script>
+import Film from './Film.vue'
+
+export default {
+    name: 'Main',
+    components: {
+        Film
+    },
+    props: {
+        films: Array,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
