81a8796307c2cf11fd355e7dd7ddfd3b28dfae05
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-07-16 16:33:25 +0200

    header e struttura

---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+    <header class="row align-items-center">
+        <div class="logo-container col-1">
+            <img src="../assets/Netflix-Logo.png" alt="Logo Netflix">
+        </div>
+        <nav class="col">
+            <ul>
+                <li v-for="item in navList" :key="item.id">{{ item.text }}</li>
+            </ul>
+        </nav>
+        <div class="search-bar col-2">
+            <font-awesome-icon icon="fa-search" class="icon-search" />
+            <font-awesome-icon icon="fa-search" />
+        </div>
+    </header>
+
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        navList: Array,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/colors';
+
+    header {
+        height: 50px;
+        
+        .logo-container {
+
+            img {
+                width: 100%;
+            }
+        }
+
+        nav {
+
+            ul {
+                display: flex;
+                align-items: center;
+                height: 100%;
+
+                li {
+                    color: $link;
+                    margin: 0 6px;
+                    font-size: 12px;
+                    font-weight: bold;
+                }
+            }
+        }
+
+        .search-bar {
+            color: $white;
+        }
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Vue from 'vue'
 import App from './App.vue'
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
